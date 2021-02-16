a010f3dbbf6c8488e4c8a0d4f3f7220a29a2bd8f
Timberzzman <leloupandco@gmail.com>
2021-02-16 19:33:33 +0100

    Adding Board functions

---
+++ src/board.js
@@ -0,0 +1,45 @@
+const board = {};
+
+function checkLines(line, matches) {
+    if (line < 0 || line > matches.length - 1) {
+        return false;
+    }
+    return true;
+}
+
+function checkMatches(input, matches) {
+    if (matches[input.line].number < input.matches
+        || matches[input.line].number - input.matches < 0) {
+        return false;
+    }
+    return true;
+}
+
+board.initiateBoard = (lines, width) => {
+    const matches = [];
+
+    matches.push({ number: 1, start: (width - 1) / 2 });
+    for (let i = 2; i <= lines; i += 1) {
+        const number = (i * 2) - 1;
+        matches.push({ number, start: (width - number) / 2 });
+    }
+    return matches;
+};
+
+board.checkInput = (input, matches) => {
+    if (!checkLines(input.line, matches)) {
+        return 'Error: this line is out of range';
+    } if (!checkMatches(input, matches)) {
+        return 'Error: matches';
+    }
+    return '';
+};
+
+board.deleteMatches = (line, number, matches) => {
+    const result = matches;
+    const leftMatches = matches[line].number - number;
+    result[line].number = leftMatches;
+    return result;
+};
+
+module.exports = board;
